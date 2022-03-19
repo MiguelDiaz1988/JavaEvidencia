af101db64de66e8b594dba932407745d891c92dd
Miguel Diaz <mdiaz@arkusnexus.com>
2022-03-19 13:08:45 -0700

    estructura inicial y validacion de usuarios

---
+++ src/citas/Usuario.java
@@ -0,0 +1,39 @@
+package citas;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+
+public class Usuario {
+    private String Usuario;
+    private String Password;
+
+    private HashMap<String, String> usuarios = new HashMap<>();
+    private String  FILE_NAME = "src\\citas\\usuarios.csv";
+
+    public Usuario(String usuario, String password) {
+        this.Usuario = usuario;
+        this.Password = password;
+        this.loadUsersFileData();
+    }
+
+    public boolean hasValidCredentials() {
+        String passwordFound =  usuarios.get(this.Usuario);
+        return passwordFound == this.Password;
+    }
+
+    private void loadUsersFileData() {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(FILE_NAME));
+            String line;
+
+            while((line = bufferedReader.readLine()) != null) {
+                String[] values = line.split(",");
+                usuarios.put(values[0], values[1]);
+            }
+        } catch (Exception ex) {
+            System.out.println("Error:" + ex.getMessage());
+        }
+    }
+}
